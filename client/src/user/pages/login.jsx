c1689196296ceb73278bdf3143012c787d898907
Irvin <irvinbarham@gmail.com>
2025-04-19 22:53:30 -0500

    adding login

--- client/src/user/pages/login.jsx
+++ client/src/user/pages/login.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react'
-import { Navigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
 
 const Login = () => {
+    const navigate = useNavigate();
     const [userName, setUserName] = useState(null);
     const [password, setPassword] = useState(null);
-    const [authCheck, setAuthCheck] = useState(false);
     const [authUsers, setAuthUsers] = useState([]);
     const [authPass, setAuthPass] = useState([]);
-    const [authorized, setAuthStatus] = useState(false);
 
     useEffect(() => {
         fetch('/authUsers')
@@ -18,28 +18,31 @@ const Login = () => {
         .then(data => {setAuthPass(data.passwords)})
     },[])
 
- 
-    const Authorize = () => {
-        const navigate = Navigate();
-        if(authUsers.includes(document.getElementById('userName').value) && authPass.includes(document.getElementById('password').value)){
+    const Authorize = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        if(authUsers.includes(userName) && authPass.includes(form.password.value)){
             alert("Youre in!");
-            return <Navigate to="/home" replace />
+            navigate('/home', {replace: true});
         }
-        else   
+        else{
             alert("Incorrect Login");
+            setUserName('');
+            setPassword('');     
+        }   
     }
 
     return(
         <div className="login">
-            <form>
+            <form onSubmit={Authorize}>
                 <h1>Irvin's Web Games</h1>
                 <label>Username:</label>
-                <input type="text" id="userName" required/>
+                <input type="text" name="userName" value={userName} onChange={user => setUserName(user.target.value)} required/>
                 <br/>
                 <label>Password:</label>
-                <input type="password" id="password" required/>
+                <input type="password" name="password" value={password} onChange={pass => setPassword(pass.target.value)} required/>
                 <br/>
-                <button type="submit" onClick={Authorize}>Login</button>
+                <button type="submit">Login</button>
             </form>
         </div>
     )

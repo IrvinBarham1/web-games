aecd6c447d7f06f81a59d23c5d58756fb86330ea
Irvin <irvinbarham@gmail.com>
2025-05-05 21:44:22 -0500

    adding game setup

--- client/src/places/components/SnakesAndLadders.jsx
+++ client/src/places/components/SnakesAndLadders.jsx
@@ -1,39 +1,100 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import "./assets/SnakesAndLadders.css";
 
 function SnakesAndLadders () {
     const [board, setBoard] = useState(Array.from({ length: 10 }, () => Array(10).fill(0)));
     const [numPlayers, setNumPlayers] = useState(2);
+    const [symbols, setSymbols] = useState([null]);
+    const [playerNames, setPlayerNames] = useState([null]);
     const [toggleSettings, setToggleSettings] = useState(true);
-    const [symbols, setSymbols] = useState([null])
-    const [playerNames, setPlayerNames] = useState(["Default Name"])
+    const [toggleDice, setToggleDice] = useState(false);
+    const [diceValue, setDiceValue] = useState(0);
+    const [positions, setPositions] = useState([null]);
 
-    useEffect(() => {
-
-    }, []);
+    const players = {
+        num: numPlayers,
+        name: playerNames,
+        symbol: symbols,
+        position: positions
+    }
 
     const hideGameSettings = () => {
         setToggleSettings(!toggleSettings);
+
     }
 
     const GameSettings = () => (
         <div>
+            <p>Game Settings</p>
             <label>Players</label>
-            <input type="number" name="players" max="6" min="2" value={numPlayers} onChange={players => setNumPlayers(players.target.value)} required />
-            <label>Set Player Symbol</label>
+            <input 
+             type="number"
+             name="players"
+             max="6" 
+             min="2" 
+             value={numPlayers} 
+             onChange={players => setNumPlayers(players.target.value)} 
+             required />
+             <br/>
+            <label>Edit Players</label>
+
             {Array.from({length:numPlayers}).map((count,index) => (
                 <ul>
-                    <li key={index}>{playerNames[index]} {index+1}<input maxLength = "10" placeholder="set name" type="text"></input>
-                    <input maxLength = "1" placeholder="set symbol"type="text" value={symbols[index]}></input></li>
+                    <li key={index}> Name: <b>{playerNames[index]}</b> Symbol: {symbols[index]}
+                        <input
+                         maxLength="10" 
+                         placeholder="Set a Name"
+                         onBlur={name => {
+                            const newNames = [...playerNames];
+                            newNames[index] = name.target.value;
+                            setPlayerNames(newNames);
+                            
+                            }}
+                        />
+                        <select onBlur={symbol => {
+                            const newSymbols = [...symbols];
+                            newSymbols[index] = symbol.target.value;
+                            setSymbols(newSymbols);
+                        }}>
+                            <option value="✦">✦</option>
+                            <option value="♣">♣</option>
+                            <option value="♥">♥</option>
+                            <option value="♚">♚</option>
+                            <option value="♞">♞</option>
+                            <option value="♛">♛</option>
+                        </select>
+                    </li>
                 </ul>
             ))}
-
-            <button type="submit" className="button-settings">Save Changes</button>
+            <p>Players: {numPlayers} and Names: {playerNames} and {symbols}</p>
         </div>
     )
 
+    const validateGameSettings = () => {
+        if(playerNames.includes(null) || symbols.includes(null))
+            alert("Please Fill Out The Game Settings")
+        else{
+            hideGameSettings()
+            hideDice();
+        }
+    }
+    
+    const hideDice = () => {
+        setToggleDice(!toggleDice);
+    }
+
+    const Dice = () => {
+        return (
+            <div>
+                <label>It's {playerNames} Turn to Roll</label>
+                <button className="button-settings" onClick={() => rollDice()}> You Rolled a {diceValue}</button>
+            </div>
+        )
+    }
+
     const rollDice = () => {
-        return Math.floor(Math.random() * 6) + 1;
+        //if 6 need to roll again
+        setDiceValue((Math.floor(Math.random() * 6) + 1))
     }
 
    const handlePlayerMove = (currentPosition) => {
@@ -44,10 +105,9 @@ function SnakesAndLadders () {
         <div className="game-container">
             <h1 className="game-name">Snakes and Ladders</h1>
             <div className="game-container-right">
-                <button className="button-settings" onClick={() => hideGameSettings()}>
-                    Show Game Settings
-                </button>
+                <button className="button-settings" onClick={() => validateGameSettings()}>Start Game</button>
                 {toggleSettings && <GameSettings/>}
+                {toggleDice && <Dice/>}
                 
             </div>
             <img className="image-board" src="/snakesladdersboard.jpg" alt="Board Game"/>
